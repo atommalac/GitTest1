f0cafb50890d13f3e992dd3001b6f965cd7435cc
atommalac <atomtengeralattjaro@gmail.com>
2016-08-18 14:36:15 +0200

    wtf
    wtf wtf

--- GitTest1Solution/GitTest1Solution/SomeClass.cs
+++ GitTest1Solution/GitTest1Solution/SomeClass.cs
@@ -9,7 +9,7 @@ namespace GitTest1Solution
     public class SomeClass
     {
         /// <summary>
-        /// Something indeed.
+        /// Something indeed. WTF
         /// </summary>
         public string Something { get; set; }
     }
