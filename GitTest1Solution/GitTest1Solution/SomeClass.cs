7e4bb769135b838398765e33960445da6f2ef0f1
atommalac <atomtengeralattjaro@gmail.com>
2016-08-18 14:04:13 +0200

    Removed unnecessary usings.

--- GitTest1Solution/GitTest1Solution/SomeClass.cs
+++ GitTest1Solution/GitTest1Solution/SomeClass.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GitTest1Solution
+﻿namespace GitTest1Solution
 {
     public class SomeClass
     {
